a36b9789975a0fbd4a79a3086767b26120184de8
jin.js <jin.js@jinjsui-MacBookPro.local>
2024-08-12 18:23:42 +0900

    modify spark test configuration

--- dags/configs.py
+++ dags/configs.py
@@ -7,12 +7,13 @@ spark_confs = {
 
     "spark.jars": "/opt/airflow/jars/*",
 
-    # DataNucleus가 테이블이 존재하지 않는 경우 자동으로 테이블을 생성
-    "spark.hadoop.datanucleus.autoCreateTables": True,
-    # DataNucleus가 테이블/스키마 존재하지 않는 경우 테이블/스키마 자동으로 생성
-    "spark.hadoop.datanucleus.schema.autoCreateTables": True,
-    # DataNucleus가 사용하는 데이터 스토어(데이터베이스 구조)가 고정되지 않음
-    "spark.hadoop.datanucleus.fixedDatastore": False,
+    # # DataNucleus가 테이블이 존재하지 않는 경우 자동으로 테이블을 생성
+    # "spark.hadoop.datanucleus.autoCreateTables": True,
+    # # DataNucleus가 테이블/스키마 존재하지 않는 경우 테이블/스키마 자동으로 생성
+    # "spark.hadoop.datanucleus.schema.autoCreateTables": True,
+    # # DataNucleus가 사용하는 데이터 스토어(데이터베이스 구조)가 고정되지 않음
+    # "spark.hadoop.datanucleus.fixedDatastore": False,
+    "spark.sql.hive.metastore.schema.verification": False, 
 
     "spark.hadoop.fs.s3a.access.key": "admin",
     "spark.hadoop.fs.s3a.secret.key": "admin1234",
@@ -26,6 +27,6 @@ spark_confs = {
     "spark.sql.parquet.mergeSchema": "true",
     "spark.sql.parquet.compression.codec": "gzip",
     "spark.sql.session.timeZone": "Asia/Seoul",
-    "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
-    "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+    # "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
+    # "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
 }
